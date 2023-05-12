5793dbda353faafbd6c76fa4de01556428262fcf
moh-qureshi <h-qureshi95@hotmail.co.uk>
2023-05-12 16:07:17 +0100

    final commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Header } from './components/Header';
 import { Home } from './components/Home';
 import { Cart } from './components/Cart';
+import React from 'react';
 
 function App() {
   return (
